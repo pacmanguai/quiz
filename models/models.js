facbaaa9a3b431142ef2873c41aa27fdef9a6385
pacmanguai <pactorre@gmail.com>
2016-08-24 12:35:00 +0200

    Add heroku DB

--- models/models.js
+++ models/models.js
@@ -1,10 +1,30 @@
 var path = require('path');
 
+//Postgres DATABASE_URL = postgres://user:passwd@host:port/database
+//SQLite   DATABASE_URL = sqlite://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name  = (url[6]||null);
+var user     = (url[2]||null);
+var pwd      = (url[3]||null);
+var protocol = (url[1]||null);
+var dialect  = (url[1]||null);
+var port     = (url[5]||null);
+var host     = (url[4]||null);
+var storage  = process.env.DATABASE_STORAGE;
+
 // Cargar modelo ORM
 var Sequelize = require('sequelize');
 
-// Usar DB SQLite
-var sequelize = new Sequelize(null, null, null, {dialect: "sqlite", storage: "quiz.sqlite"});
+// Usar DB SQLite o Postgres
+var sequelize = new Sequelize(DB_name, user, pwd, 
+	{	dialect:  protocol, 
+		protocol: protocol,
+		port:	  port,
+		host:	  host,
+		storage:  storage, 	// solo SQLite (.env)
+		omitNull: true		// solo Postgres		
+	}
+);
 
 // Importar la definición de la tabla Quiz
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
@@ -20,7 +40,7 @@ sequelize.sync().success(function() {
 			Quiz.create({ 	pregunta: 'Capital de Italia',
 							respuesta: 'Roma'
 						})
-				.success(function() {console.log('Base de datos inicilizada')});
+				.success(function() {console.log('Base de datos inicializada')});
 		};
 	});
 });
