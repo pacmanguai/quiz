77ff1af2f76208e9f2e0279c7935728734c9cf0d
pacmanguai <pactorre@gmail.com>
2016-08-26 11:51:23 +0200

    Comment moderation

--- models/comment.js
+++ models/comment.js
@@ -1,10 +1,14 @@
 // Definición del modelo de comment con validación
 
 module.exports = function(sequelize, DataTypes) {
-	return sequelize.define('Comment',{	
-		texto: {	
+	return sequelize.define('Comment',{
+		texto: {
 			type: DataTypes.STRING,
 			validate: { notEmpty: { msg: "-> Falta comentario"}}
+		},
+		publicado: {
+		    type: DataTypes.BOOLEAN,
+		    defaultValue: false
 		}
-	});	
+	});
 }
--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,7 @@ router.get('/', function(req, res) {
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load);
+router.param('commentId', commentController.load);
 
 // Definición de las rutas de sesión
 router.get('/login',    sessionController.new);
@@ -31,5 +32,6 @@ router.delete('/quizes/:quizId(\\d+)', 		sessionController.loginRequired, quizCo
 //Definición de las rutas de comments
 router.get('/quizes/:quizId(\\d+)/comments/new', 	commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', 		commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 
 module.exports = router;
